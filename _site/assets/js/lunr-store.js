4acb803d4e16b4f53155f51ea557329c6f66ec04
Schudlich <schudlic@umich.edu>
2025-11-22 19:56:55 -0500

    first exec

---
+++ _site/assets/js/lunr-store.js
@@ -0,0 +1,137 @@
+var store = [{  
+"title": "Administration Building, University of Idaho, No. 30","date": "1910","creator": "Pacific Photo Co.","description": "Example locally hosted image item. Photographic postcard of the University of Idaho administration building in Moscow, Idaho.","subject": "universities; buildings; campuses; picture postcards","location": "Moscow, Idaho", 
+"id": "demo_001.html"
+
+},{  
+"title": "Spokane County Court House, Spokane, Washington","date": "1912-09-08","creator": "Spokane Post Card Co.","description": "Example locally hosted PDF item. Postcard is of the Spokane County Courthouse in Spokane, Washington. Postmark 9/8/1912, Spokane, WA. Destination San Francisco, CA.","subject": "public buildings; county courthouses; trees; picture postcards","location": "Spokane, Washington", 
+"id": "demo_002.html"
+
+},{  
+"title": "Good News – Power (Radio Episode Excerpt)","date": "1947","creator": "Robinson, Frank B.","description": "Example locally hosted audio item. A short excerpt from a Psychiana radio program episode that begins with assurance that there is a Power that can get people what they desire. Robinson then discusses his early life failures and childhood seeking of some force that could turn his failures into success. He asserts the God-Power can turn our failures into success and bring us material and spiritual abundance.","subject": "Frank B. Robinson; Failure; Success; Material Abundance","location": "Moscow, Idaho", 
+"id": "demo_003.html"
+
+},{  
+"title": "University of Idaho vs. University of Southern California (Football), 10/30/1925","date": "1925-10-30","creator": "Vandal Athletics","description": "Example YouTube video item. Idaho Football vs. University of Southern California 10/30/1925 at Neale Stadium in Moscow, ID. Score: 7 - 51 (L). Silent film footage.","subject": "American Football","location": "Moscow, Idaho", 
+"id": "demo_004.html"
+
+},{  
+"title": "Interview with K. Silem Mohammad","date": "2014-06-16","creator": "Becker, Devin","description": "Example Vimeo video item. Oral history interview with poet K. Silem Mohammad discussing recent changes in writing practices that occurred due to the advent of the computer and the arrival of the digital age.","subject": "Poetry","location": "Ashland, OR", 
+"id": "demo_005.html"
+
+},{  
+"title": "Ford pumper used for slash burning control","date": "1932-09-01","creator": "Thompson, J. B.","description": "Example image item externally hosted. Ford pumper used for slash burning control. September 1932. From the photo series depicting the broadcast burn at the mouth of Benton Creek.","subject": "forestry","location": "Bonner County;Priest River Experimental Forest;Benton Creek", 
+"id": "demo_006.html"
+
+},{  
+"title": "Influence of Fishway Placement on Fallback of Adult Salmon at the Bonneville Dam on the Columbia River","date": "2003","creator": "Reischel, T.S.; Bjornn, T.C.","description": "Example metadata-only record with link to external source. Journal article.","subject": "fisheries management","location": "Bonneville Dam", 
+"id": "demo_007.html"
+
+},{  
+"title": "Hell's Half Acre","creator": "Keeping Watch","description": "Example compound object. Hells Half Acre is an R-6 cabin that is easily accessible by road in the Selway-Bitterroot Wilderness. Originally a platform on top of a forty-foot-tall tower, the original Hell's Half Acre cabin was an L-4. Now one of the most popular lookouts in Idaho or Montana, it sits above a snag forest of mostly lodgepole pine. A sign at the base of the summit informs drivers that the tower opens at 9 am. Unsure if this refers to mountain or pacific time, we wait at the sign and drink our coffee.", 
+"id": "demo_008.html"
+
+},{  
+"title": "Hell's Half Acre Lookout 360 Image","date": "2021-07-13","creator": "Keeping Watch","description": "Example child object. 360 Image of the Interior of Hell's Half Acre Fire Tower","location": "Hell's Half Acre", 
+"id": "demo_008.html#demo_009"
+
+},{  
+"title": "Patrick McMarron Records Fire Conditions at Hell's Half Acre Lookout","date": "2021-07-13","creator": "Keeping Watch","description": "Example child object. Video of Patrick McMarron recording fire conditions in July of 2021 on Hells Half Acre Fire Lookout","location": "Hell's Half Acre", 
+"id": "demo_008.html#demo_010"
+
+},{  
+"title": "Outside shot of Hells Half Acre Lookout","date": "2021-07-13","creator": "Keeping Watch","description": "Example child object. Outside shot of Hells Half Acre Lookout","location": "Hell's Half Acre", 
+"id": "demo_008.html#demo_011"
+
+},{  
+"title": "Patrick McMarron Discusses the Importance of Human Beings on the Lookout","date": "2021-07-13","creator": "Keeping Watch","description": "Example child object. Video of Patrick McMarron discussing the importance of human beings staffing fire lookouts","subject": "Patrick McMarron; Hell's Half Acre","location": "Hell's Half Acre", 
+"id": "demo_008.html#demo_012"
+
+},{  
+"title": "Peeled Tree","description": "Example multiple. A cross section of a peeled tree from the Payette National Forest. The inner bark was utilized as a food resource by Indigenous Tribe.","subject": "Peeled Tree; Payette National Forest; Indigenous Tribe", 
+"id": "demo_013.html"
+
+},{  
+"title": "Peeled Tree View 1", 
+"id": "demo_013.html#demo_014"
+
+},{  
+"title": "Peeled Tree View 2", 
+"id": "demo_013.html#demo_015"
+
+},{  
+"title": "Peeled Tree View 3", 
+"id": "demo_013.html#demo_016"
+
+},{  
+"title": "Hell's Half Acre Lookout 360 Image","date": "2021-07-13","creator": "Keeping Watch","description": "Example panorama. 360 Image of the Interior of Hell's Half Acre Fire Tower","location": "Hell's Half Acre", 
+"id": "demo_017.html"
+
+},{  
+"title": "Spokane's Great Restaurant, Washington","date": "1909","creator": "Mitchell, Edward H.","description": "Example postcard using display template 'multiple.' Postcard is of the Davenport Hotel's Restaurant in Spokane, Washington.","subject": "restaurants; automobiles; buildings; picture postcards; flagpoles; flags","location": "Spokane, WA", 
+"id": "demo_018.html"
+
+},{  
+"title": "postcard front", 
+"id": "demo_018.html#demo_019"
+
+},{  
+"title": "postcard back", 
+"id": "demo_018.html#demo_020"
+
+},{  
+"title": "Jennie Eva Hughes, the First Black Graduate of the University of Idaho","date": "1899","description": "Example compound object. Items from the Black History at University of Idaho that feature Jennie Eva Hughes, the first black graduate of the University of Idaho","subject": "Jennie Eva Hughes; Black History; University of Idaho","location": "Moscow, ID", 
+"id": "demo_021.html"
+
+},{  
+"title": "Portrait of Jennie Eva Hughes [1]","date": "1899","description": "Example child object. Black and white portrait of Jennie Eva Hughes.", 
+"id": "demo_021.html#demo_022"
+
+},{  
+"title": "Portrait of Jennie Eva Hughes [2]","date": "1899","description": "Example child object. Black and white standing portrait of Jennie Eva Hughes dressed in white. Hughes holds a scroll in her left hand.", 
+"id": "demo_021.html#demo_023"
+
+},{  
+"title": "\"The Uncrowned King\" by Jennie Eva Hughes","date": "1898-04-15","creator": "Hughes, Jennie Eva","description": "Example child object. This speech won her the medal oration on April 15, 1898 at University of Idaho.", 
+"id": "demo_021.html#demo_024"
+
+},{  
+"title": "Spokesman article about Jennie Eva Hughes' oration win","date": "1898","creator": "The Spokesman-Review","description": "Example child object. Spokesman-Review article about the oratorical event event and Jennie Eva Hughes' win.", 
+"id": "demo_021.html#demo_025"
+
+},{  
+"title": "University of Idaho class of 1899","date": "1898","description": "Example child object. Back row (left to right): Fred Merriam, Clara Playfair, Eva Nichols, Clem Herbert, Guy Wolfe, Ava Sweet, Fred Moore. Front row (left to right): Kate Hanley, Maude Mix, Charles Armstrong, Will Stillinger, Jennie Eva Hughes, Gilbert Hogue.", 
+"id": "demo_021.html#demo_026"
+
+},{  
+"title": "Jennie Eva Hughes' 1898 registration card at University of Idaho","date": "1898-09-23","description": "Example child object. Jennie Eva Hughes' registration card for 1898 for the University of Idaho.", 
+"id": "demo_021.html#demo_027"
+
+},{  
+"title": "Jennie Eva Hughes' 1899 registration card at University of Idaho","date": "1899-02-15","description": "Example child object. Jennie Eva Hughes' registration card for 1899 for the University of Idaho.", 
+"id": "demo_021.html#demo_028"
+
+},{  
+"title": "Innovation of Class Day","date": "1899-06-01","description": "Example child object. Argonaut article about Class Day with mention of the oration given by Jennie Eva Hughes.", 
+"id": "demo_021.html#demo_029"
+
+},{  
+"title": "The First Black Graduate - Jennie Eva Hughes","date": "2022","creator": "Shannon, Michelle","description": "Example child object. Article summarizing the history of Jennie Eva Hughes at the Univeritiy of Idaho.", 
+"id": "demo_021.html#demo_030"
+
+},{  
+"title": "Mounted forest patrol looks across Middle Fork of Salmon River","date": "1937","creator": "Benedict, M.S.","description": "Mounted forest patrol looks across Middle Fork of Salmon River in the Salmon-Challis National Forest.","subject": "national forests; horses; rivers; riders; rangers; patrol; campsite; camp fire","location": "Salmon-Challis National Forest, Idaho", 
+"id": "demo_031.html"
+
+},{  
+"title": "Combined harvester, Moscow, Idaho","date": "1920","creator": "Inland Printing Co.","description": "Postcard of a combine harvesting grain in fields near Moscow, Idaho.","subject": "combines; fields (land); Equus caballus (species); men; picture postcards","location": "Moscow, Idaho", 
+"id": "demo_032.html"
+
+},{  
+ 
+"id": "demo_032.html#demo_033"
+
+},{  
+ 
+"id": "demo_032.html#demo_034"
+
+}];
